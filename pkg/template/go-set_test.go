e569b6fc3a5a6f7d5f2c51c7766a33a1b09f48f5
rithvik <innovation6178@gmail.com>
2023-11-13 16:07:33 +0530

    test args modified according to function

--- pkg/template/go-set_test.go
+++ pkg/template/go-set_test.go
@@ -10,7 +10,7 @@ func TestCreateDeepCopy(t *testing.T) {
 }
 
 func TestCloneProject(t *testing.T) {
-	cloneProject("boot", "../../", "https://github.com/wangyoucao577/go-project-layout.git")
+	CloneProject("boot1", "/home/ra/Documents", "https://github.com/wangyoucao577/go-project-layout.git")
 }
 
 func TestRemoveNonEmptyDir(t *testing.T) {
