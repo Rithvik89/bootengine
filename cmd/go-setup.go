2ae742597b45569127988764aec0ec04eb4a2493
rithvik <innovation6178@gmail.com>
2023-11-25 18:43:10 +0530

    flags cloudProvider & localInfra added to subcmd go

--- cmd/go-setup.go
+++ cmd/go-setup.go
@@ -8,6 +8,8 @@ import (
 
 var source string
 var dest string
+var cloudProvider string
+var localInfra string
 
 var goCmd = &cobra.Command{
 	Use:   "go",
@@ -23,4 +25,6 @@ func init() {
 	rootCmd.AddCommand(goCmd)
 	goCmd.Flags().StringVarP(&source, "source", "s", "", "This is the source path from which bootengine to take reference. ")
 	goCmd.Flags().StringVarP(&dest, "destination", "d", "", "Destination where the desired project setup is to be made. ")
+	goCmd.Flags().StringVarP(&cloudProvider, "cloud", "cld", "", "File Consisting of Cloud Providers which will be used in the project. ")
+	goCmd.Flags().StringVarP(&localInfra, "localinfra", "li", "", "File Consisting of Driver and services used for running it locally.")
 }
